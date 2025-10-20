d64f8a35028bca4b8039f250455c0c5b5e4fc80e
MirkoTorrejon <mirko.torrejon@jala.university>
2025-10-20 17:27:34 -0400

    Suspense adding

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,15 +1,18 @@
 import AdvancedSearchForm from "@/components/AdvancedSearchForm";
 import BooksDisplayer from "@/components/BooksDisplayer";
 import styles from "./page.module.css";
+import { Suspense } from "react";
 
 export default function Search() {
   return (
     <div className={styles.container}>
-      <div>
+      <Suspense fallback={<div>Cargando...</div>}>
+        <div>
         <h1 className={styles.title}>Advanced search</h1>
         <AdvancedSearchForm></AdvancedSearchForm>
       </div>
       <BooksDisplayer></BooksDisplayer>
+      </Suspense>
     </div>
   );
 }
